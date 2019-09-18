b957c752e00b170360d98fb6e66cf597da3311e1
nyoronyoro111 <yuma.maron_1026@icloud.com>
2019-09-18 22:16:54 +0900

    今度こそ...

--- mypage/urls.py
+++ mypage/urls.py
@@ -21,9 +21,9 @@ urlpatterns = [
     path('setting/jobhunting/startTime',views.jobHunting_startTime_view.as_view(), name='jobhunting_startTime'),
     path('setting/jobhunting/requestment',views.jobHunting_requestment_view.as_view(), name='jobhunting_requestment'),
     path('setting/jobhunting/internship/create',views.internshipRec_create.as_view(), name='jobhunting_internRec_create'),
-    path('setting/jobhunting/internship/update',views.internshipRec_update.as_view(), name='jobhunting_internRec_update'),
-    path('setting/jobhunting/internship/delete',views.internshipRec_delete.as_view(), name='jobhunting_internRec_delete'),
+    path('setting/jobhunting/internship/update/<uuid:id>',views.internshipRec_update.as_view(), name='jobhunting_internRec_update'),
+    path('setting/jobhunting/internship/delete/<uuid:id>',views.internshipRec_delete.as_view(), name='jobhunting_internRec_delete'),
     path('setting/jobhunting/prospectiveEmployer/create',views.prospectiveEmployer_create.as_view(), name='jobhunting_prospectiveEmployer_create'),
-    path('setting/jobhunting/prospectiveEmployer/update',views.prospectiveEmployer_update.as_view(), name='jobhunting_prospectiveEmployer_update'),
-    path('setting/jobhunting/prospectiveEmployer/delete',views.prospectiveEmployer_delete.as_view(), name='jobhunting_prospectiveEmployer_delete'),
+    path('setting/jobhunting/prospectiveEmployer/update/<uuid:id>',views.prospectiveEmployer_update.as_view(), name='jobhunting_prospectiveEmployer_update'),
+    path('setting/jobhunting/prospectiveEmployer/delete/<uuid:id>',views.prospectiveEmployer_delete.as_view(), name='jobhunting_prospectiveEmployer_delete'),
 ]
